937ebc86fccf842e1251cd1eb48495132d33db86
Naresh jupalle <naresh.jupalle410@gmail.com>
2024-12-17 22:10:51 +0530

    How js would execute in browser

---
+++ basics/running-javascript.js
@@ -0,0 +1,6 @@
+
+const userNames  = ['user1','user2']
+
+
+
+// when you have moduler approach & trying to access the variable in console it doesn't show there as it restricted to that specific file.
---
+++ module-one.js
@@ -0,0 +1,1 @@
+export const userNames  = ['user1','user2']
